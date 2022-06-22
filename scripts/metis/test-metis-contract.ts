426db8f30bcc4db3615c23506ffc76970e31fb71
cehali <cezary.haliniarz@gmail.com>
2022-06-22 13:36:02 +0200

    feat: add script and check integration with Metis

---
+++ scripts/metis/test-metis-contract.ts
@@ -0,0 +1,21 @@
+import { ethers } from "hardhat";
+import { WrapperBuilder } from "redstone-evm-connector";
+import { ExampleRedstoneRapid as ExampleRedstoneRapidType } from "../../typechain/ExampleRedstoneRapid";
+
+(async () => {
+  const ExampleRedstoneRapid = await ethers.getContractFactory("ExampleRedstoneRapid");
+  const contract = await ExampleRedstoneRapid.deploy();
+
+  console.log(`Contract address: ${contract.address}`);
+
+  const wrappedContract = WrapperBuilder
+    .wrapLite<ExampleRedstoneRapidType>(contract as ExampleRedstoneRapidType)
+    .usingPriceFeed("redstone-rapid", {
+      asset: "ETH",
+    });
+  
+  const setPriceTransaction = await wrappedContract.setPrice({ gasLimit: "55000" });
+  console.log(`Set price transaction hash: ${setPriceTransaction.hash}`);
+  const priceFromContract = await wrappedContract.getLastPrice();
+  console.log(priceFromContract)
+})();
