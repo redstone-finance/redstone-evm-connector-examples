ae7e797a96b3f59ec5b94bf863b412e6ce920e07
hatskier <suvorovalex1997@gmail.com>
2022-08-05 14:56:45 +0200

    feat: added examples with  token price

--- test/example-avalanche-prod.test.js
+++ test/example-avalanche-prod.test.js
@@ -25,4 +25,24 @@ describe("Example avalanche prod", function () {
     const priceFromContract = await exampleContract.getLastPrice();
     console.log({ priceFromContract: priceFromContract.toNumber() });
   });
+
+  it("YY price test - single price", async function () {
+    const yySymbol = "$YYAV3SA1";
+    exampleContract = WrapperBuilder
+      .wrapLite(exampleContract)
+      .usingPriceFeed("redstone-avalanche-prod", { asset: yySymbol });
+    const bytes23Symbol = ethers.utils.formatBytes32String(yySymbol);
+    const yyValueFromContract = await exampleContract.getPriceSecurely(bytes23Symbol);
+    console.log({ [yySymbol]: yyValueFromContract.toNumber() });
+  });
+
+  it("YY price test - with package", async function () {
+    const yySymbol = "$YYAV3SA1";
+    exampleContract = WrapperBuilder
+      .wrapLite(exampleContract)
+      .usingPriceFeed("redstone-avalanche-prod");
+    const bytes23Symbol = ethers.utils.formatBytes32String(yySymbol);
+    const yyValueFromContract = await exampleContract.getPriceSecurely(bytes23Symbol);
+    console.log({ [yySymbol]: yyValueFromContract.toNumber() });
+  });
 });
