3c32b5c8f71c6e8020320df1f5cd895d8ec89d31
hatskier <suvorovalex1997@gmail.com>
2022-08-09 14:34:54 +0200

    chore: custom url example adjusted

--- test/example-custom-urls-demo.test.js
+++ test/example-custom-urls-demo.test.js
@@ -12,8 +12,8 @@ describe("Example Custom URL", function () {
     exampleContract = WrapperBuilder
       .wrapLite(exampleContract)
       .usingPriceFeed("redstone-custom-urls-demo", {
-        // Check more details at: https://custom-urls-manifest-updater.redstone.finance/0x031f7bcd73d9f5ed
-        asset: "0x031f7bcd73d9f5ed",
+        // Check more details at: https://custom-urls-manifest-updater.redstone.finance/0x60cbe6b18347697f
+        asset: "0x60cbe6b18347697f",
       });
     const valueFromOracle = await exampleContract.getValue();
     console.log({ valueFromOracle: valueFromOracle.toNumber() });
